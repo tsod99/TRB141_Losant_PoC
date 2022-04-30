a8f9be933f21c199e9dad803e6d05b24fd1fa05f
Abdurahman <abda99abda99@gmail.com>
2022-04-30 15:34:55 +0200

    Update eea-registered-functions.js

--- Project/src/eea-registered-functions.js
+++ Project/src/eea-registered-functions.js
@@ -1,9 +1,12 @@
 const { encodeStringAndLength, decodeString, EEA_RESULT_CODES } = require('./eea-utils');
+const { exec } = require('child_process');
 const modbus = require('./eea-modbus');
 
-var modbusData = {};
+var modbusData_IO = {};
+var modbusData_Main = {};
+var data_updated = 0;
 
-//////////// Default Modbus Register Addresses /////////////////////////////////
+//////////// Default I/O Modbus Register Addresses /////////////////////////////////
 // PIN 3 status (4 PIN connector)           324
 // PIN 4 status (4 PIN connector)           325
 // PIN 3 direction                          326
@@ -15,70 +18,111 @@ var modbusData = {};
 // PIN 2 input status                       342
 // PIN 1 (Dry/Wet) status                   343
 // PIN 2 (Dry/Wet) status                   344
-// WAN IP Reg 1                             139
-// WAN IP Reg 2                             140
 ////////////////////////////////////////////////////////////////////////////////
 
 const combine2Registers = (reg1, reg2) => {
  return parseInt(reg1)*65536 + parseInt(reg2);
 }
 
-async function getModbusData()
+async function getIOModbusData()
 {
 
   await modbus.getModbusData(324, 4)
       .then(function(res1){
         if(res1 != null){ 
-          modbusData["PIN 3 status"] = res1[2];
-          modbusData["PIN 4 status"] = res1[3];
-          modbusData["PIN 3 direction"] = res1[0];
-          modbusData["PIN 4 direction"] = res1[1];
+          modbusData_IO["pin3_status"] = res1[2];
+          modbusData_IO["pin4_status"] = res1[3];
+          modbusData_IO["pin3_direction"] = res1[0];
+          modbusData_IO["pin4_direction"] = res1[1];
         }
   });
 
   await modbus.getModbusData(337, 8)
     .then(function(res2){
       if(res2 != null){
-        modbusData["relay 1"] = res2[0];
-        modbusData["relay 2"] = res2[1];
-        modbusData["Isolated input"] = res2[2];
-        modbusData["PIN 1 input status"] = res2[4];
-        modbusData["PIN 2 input status"] = res2[5];
-        modbusData["PIN 1 (Dry/Wet) status"] = res2[6];
-        modbusData["PIN 2 (Dry/Wet) status"] = res2[7];
-        console.log(modbusData);
+        modbusData_IO["relay_1"] = res2[0];
+        modbusData_IO["relay_2"] = res2[1];
+        modbusData_IO["isolated_input"] = res2[2];
+        modbusData_IO["pin1_input_status"] = res2[4];
+        modbusData_IO["pin2_input_status"] = res2[5];
+        modbusData_IO["pin1_dry_wet_status"] = res2[6];
+        modbusData_IO["pin2_dry_wet_status"] = res2[7];
+        data_updated = 1;
       }
   });
 }
 
-module.exports = (wasmMemory) => {
+async function getMainModbusData(startAddress, count)
+{
+
+  await modbus.getModbusData(startAddress, count)
+      .then(function(res3){
+        if(res3 != null){
+          for(var i=0;i<count;i++) {
+            modbusData_Main[String(startAddress+i)] = res3[i];
+          }
+          data_updated = 2;
+        }
+  });
+}
+
+const getDataUpdated = () => {
+  return data_updated;
+};
+
+const getMainData = () => {
+  return modbusData_Main;
+};
+
+const getIOData = () => {
+  return modbusData_IO;
+};
+
+const resetDataUpdated = () => {
+  data_updated = 0;
+};
+
+const registeredFunctions = (wasmMemory) => {
   return {
-    eea_fn_read_modbus: async (outPtrReadingsStr, strBufferLen, outPtrReadingsLen) => {             
-      await getModbusData();
+    eea_fn_read_modbus_io: ( ) => {
+      modbusData_IO = {};            
+      getIOModbusData();
       return EEA_RESULT_CODES.success;
     },
 
-    eea_fn_send_modbus: (outPtrReadingsStr, strBufferLen, outPtrReadingsLen) => {        
-      encodeStringAndLength(wasmMemory, JSON.stringify(modbusData), outPtrReadingsStr, strBufferLen, outPtrReadingsLen);
+    eea_fn_read_modbus_main: (startAddress, count) => {
+      modbusData_Main = {};      
+      getMainModbusData(startAddress, count);
       return EEA_RESULT_CODES.success;
     },
 
-    eea_fn_toggle_relay: (relayNum) => {
-      console.log("Toggle relay: " + String(relayNum));
-      if(relayNum === 1){
-        if(typeof modbusData["relay 1"] !== "undefined"){ 
-          var newValue = 1 - modbusData["relay 1"];
-          modbus.writeModbusData(337, newValue);
-          modbusData["relay 1"] = newValue;
+    eea_fn_relay_on: (relayNum) => {
+
+      var cmd = "ubus call ioman.relay.relay" + String(relayNum - 1) + " update '{\"state\":\"closed\"}'";
+      exec(`${cmd}`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err)
+        } else {
+         console.log(`stdout: ${stdout}`);
+         console.log(`stderr: ${stderr}`);
         }
-      }
-      else if(relayNum === 2){
-        if(typeof modbusData["relay 2"] !== "undefined"){ 
-          var newValue = 1 - modbusData["relay 2"];
-          modbus.writeModbusData(338, newValue); 
-          modbusData["relay 2"] = newValue;
+      });
+
+      return EEA_RESULT_CODES.success;
+    },
+
+    eea_fn_relay_off: (relayNum) => {
+
+      var cmd = "ubus call ioman.relay.relay" + String(relayNum - 1) + " update '{\"state\":\"open\"}'";
+      exec(`${cmd}`, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err)
+        } else {
+         console.log(`stdout: ${stdout}`);
+         console.log(`stderr: ${stderr}`);
         }
-      }
+      });
+
       return EEA_RESULT_CODES.success;
     },
 
@@ -90,3 +134,11 @@ module.exports = (wasmMemory) => {
     }
   };
 };
+
+module.exports = {
+  getDataUpdated,
+  getMainData,
+  getIOData,
+  resetDataUpdated,
+  registeredFunctions
+}
